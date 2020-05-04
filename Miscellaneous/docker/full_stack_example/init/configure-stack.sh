f4851e496ceed4c1c3ce8fa197be55d144f4a203
cruizba <charlesrb94@outlook.com>
2020-05-04 16:16:28 +0000

    First version. Securized and filebeat working on 6.8.0

--- Miscellaneous/docker/full_stack_example/init/configure-stack.sh
+++ Miscellaneous/docker/full_stack_example/init/configure-stack.sh
@@ -2,38 +2,16 @@
 
 
 # Wait for Elasticsearch to start up before doing anything.
-until curl -u elastic:changeme -s http://elasticsearch:9200/_cat/health -o /dev/null; do
+until curl -u elastic:${ES_PASSWORD} -s http://elasticsearch:9200/_cat/health -o /dev/null; do
     echo Waiting for Elasticsearch...
     sleep 1
 done
-echo "Setting password to ${ES_PASSWORD}"
-curl -s -XPUT -u elastic:changeme 'elasticsearch:9200/_xpack/security/user/elastic/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
-curl -s -XPUT -u elastic:${ES_PASSWORD} 'elasticsearch:9200/_xpack/security/user/kibana/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
-curl -s -XPUT -u elastic:${ES_PASSWORD} 'elasticsearch:9200/_xpack/security/user/logstash_system/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
 
 # Wait for Kibana to start up before doing anything.
 until curl -s http://kibana:5601/login -o /dev/null; do
     echo Waiting for Kibana...
     sleep 1
 done
-
-# Import the standard Beats dashboards.
-/usr/share/metricbeat/scripts/import_dashboards \
-  -beat '' \
-  -file /usr/share/metricbeat/beats-dashboards-${ELASTIC_VERSION}.zip \
-  -es http://elasticsearch:9200 \
-  -user elastic \
-  -pass ${ES_PASSWORD}
-
 
 # Set the default index pattern.
 curl -s -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/.kibana/config/${ELASTIC_VERSION} \

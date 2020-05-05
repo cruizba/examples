fc0a8928acfd133f789025cf83eb24b7aaad8b6f
cruizba <charlesrb94@outlook.com>
2020-05-05 10:21:16 +0200

    Logs working

--- Miscellaneous/docker/full_stack_example/init/configure-stack.sh
+++ Miscellaneous/docker/full_stack_example/init/configure-stack.sh
@@ -12,30 +12,3 @@ until curl -s http://kibana:5601/login -o /dev/null; do
     echo Waiting for Kibana...
     sleep 1
 done
-
-# Set the default index pattern.
-curl -s -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/.kibana/config/${ELASTIC_VERSION} \
-     -d "{\"defaultIndex\" : \"${DEFAULT_INDEX_PATTERN}\"}"
-
-# Load any declared ingest pipelines
-PIPELINES=/usr/local/bin/pipelines/*.json
-for f in $PIPELINES
-do
-     filename=$(basename $f)
-     pipeline_id="${filename%.*}"
-     echo "Loading $pipeline_id ingest chain..."
-     curl -s  -H 'Content-Type: application/json' -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/_ingest/pipeline/$pipeline_id -d@$f
-done
-
-# Load any declared extra index templates
-TEMPLATES=/usr/local/bin/templates/*.json
-for f in $TEMPLATES
-do
-     filename=$(basename $f)
-     template_id="${filename%.*}"
-     echo "Loading $template_id template..."
-     curl -s  -H 'Content-Type: application/json' -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/_template/$template_id -d@$f
-     #We assume we want an index pattern in kibana
-     curl -s -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/.kibana/index-pattern/$template_id-* \
-     -d "{\"title\" : \"$template_id-*\",  \"timeFieldName\": \"@timestamp\"}"
-done
